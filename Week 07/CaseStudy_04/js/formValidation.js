51d3b7fe69ff587c714f71dec973107d920afa02
Lynn <57570218+tangminhanh@users.noreply.github.com>
2024-09-24 10:21:57 +0800

    cs4 update

--- Week 07/CaseStudy_04/js/formValidation.js
+++ Week 07/CaseStudy_04/js/formValidation.js
@@ -1,75 +1,133 @@
-// Validate Name
-function chkName(event) {
-    var myName = event.currentTarget;
-    var pos = myName.value.search(/^[A-Za-z ]+$/);
+// Error messages
+const errorName = document.getElementById('error-name')
+const errorEmail = document.getElementById('error-mail')
+const errorDate = document.getElementById('error-date')
+const errorExp = document.getElementById('error-exp')
+const errorForm = document.getElementById('error-form')
+//input field form
+const form = document.getElementById('jobForm')
+const nameContent = document.getElementById('myName');
+const emailContent = document.getElementById('myEmail');
+const dateContent = document.getElementById('startDate');
+const expContent = document.getElementById('myExp');
+const submitButton = document.getElementById('submitBtn');
 
-    if (pos !== 0) {
-        alert("The name you entered (" + myName.value +
-            ") is not in the correct form. \n" +
-            "Name should contain alphabet characters and spaces only.");
-        myName.focus();
-        myName.select();
-        return false;
+nameContent.addEventListener('blur', (event) => {
+    if (nameContent.value === '') {
+        errorName.textContent = 'This field is compulsory!'
+    } else if (isValidName(nameContent.value)) {
+        errorName.textContent = ''
+    } else {
+        errorName.textContent = 'Enter characters and whitespaces only!'
     }
-    return true;
+})
+
+/**
+ * Listens to the <email> field and show error message on blur, if applicable.
+ */
+emailContent.addEventListener('blur', (event) => {
+    if (emailContent.value === '') {
+        errorEmail.textContent = 'This field is compulsory!'
+    } else if (isValidEmail(emailContent.value)) {
+        errorEmail.textContent = ''
+    } else {
+        errorEmail.textContent = 'Your email contains invalid symbols!'
+    }
+})
+
+/**
+ * Listens to the <date> field and show error message on blur, if applicable.
+ */
+dateContent.addEventListener('blur', (event) => {
+    if (isValidStartDate(dateContent.value)) {
+        errorDate.textContent = ''
+    } else {
+        errorDate.textContent = 'You cannot start before today!'
+    }
+})
+
+/**
+ * Listens to the <experience> field and show error message on blur, if
+ * applicable.
+ */
+expContent.addEventListener('blur', (event) => {
+    if (expContent.value === '') {
+        errorExp.textContent = 'This field is compulsory!'
+    } else if (isValidExperience(expContent.value)) {
+        errorExp.textContent = ''
+    } else {
+        errorExp.textContent = 'You need to indicate your work experience!'
+    }
+})
+
+/**
+ * Validate everything before submission just in case prof asks
+ */
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    if (!isValidName(nameContent.value) ||
+        !isValidEmail(emailContent.value) ||
+        !isValidStartDate(dateContent.value) ||
+        !isValidExperience(expContent.value)) {
+        errorForm.textContent = 'Fields contain invalid data!'
+    } else {
+        nameContent.value = nameContent.value.trim()
+        emailContent.value = emailContent.value.trim()
+        expContent.value = expContent.value.trim()
+        form.submit()
+    }
+})
+
+// // Clear error messages
+// function clearErrors() {
+//     errorName.textContent = '';
+//     errorEmail.textContent = '';
+//     errorDate.textContent = '';
+//     errorExp.textContent = '';
+// }
+
+// Validation functions
+function isValidName(value) {
+    const rule = /^[a-zA-Z ]+$/
+    value.trim()
+    return (rule.test(value))
 }
 
-// Validate Email
-function chkEmail(event) {
-    var myEmail = event.currentTarget;
-    var pos = myEmail.value.search(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-
-    if (pos !== 0) {
-        alert("The email you entered (" + myEmail.value + ") is not in the correct form.");
-        myEmail.focus();
-        myEmail.select();
-        return false;
-    }
-    return true;
+function isValidEmail(value) {
+    const rule = /^[\w][\w.-]*@([\w][\w-]*\.){1,3}[a-zA-Z]{2,3}$/
+    value.trim()
+    return (rule.test(value))
 }
 
-// Validate Start Date
-function chkDate(event) {
-    var startDate = event.currentTarget;
-    var now = new Date();
-
-    var dateFormatted = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
-    var monthFormatted = (now.getMonth() + 1) < 10 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1;
-    var nowDate = now.getFullYear() + "-" + monthFormatted + "-" + dateFormatted;
-
-    if (startDate.value <= nowDate) {
-        alert("The start date cannot be from today or the past.");
-        startDate.focus();
-        startDate.select();
-        return false;
+function isValidStartDate(value) {
+    if (value !== '') {
+        const date = Date.parse(value)
+        const today = new Date()
+        return (date > today)
+    } else {
+        return true
     }
-    return true;
 }
 
-// Validate Experience
-function chkExp(event) {
-    var myExp = event.currentTarget;
-    if (myExp.value.length === 0) {
-        alert("Experience cannot be empty.");
-        myExp.focus();
-        myExp.select();
-        return false;
-    }
-    return true;
+function isValidExperience(value) {
+    return value.length > 0;
 }
+
+
 
 // Main form validation
-function formCheck() {
-    var nameField = document.getElementById('myName');
-    var emailField = document.getElementById('myEmail');
-    var dateField = document.getElementById('startDate');
-    var expField = document.getElementById('myExp');
+// function formCheck() {
+//     var nameContent = document.getElementById('myName');
+//     var emailContent = document.getElementById('myEmail');
+//     var dateContent = document.getElementById('startDate');
+//     var expContent = document.getElementById('myExp');
 
-    if (!chkName({ currentTarget: nameField }) ||
-        !chkEmail({ currentTarget: emailField }) ||
-        !chkDate({ currentTarget: dateField }) ||
-        !chkExp({ currentTarget: expField })) {
-        return false;
-    }
-    return true;
-}
+//     if (!chkName({ currentTarget: nameContent }) ||
+//         !chkEmail({ currentTarget: emailContent }) ||
+//         !chkDate({ currentTarget: dateContent }) ||
+//         !chkExp({ currentTarget: expContent })) {
+//         return false;
+//     }
+//     return true;
+// }
